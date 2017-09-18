64995402b063b00d9378529c80c961a0632a8789
cklwblove <lwbhtml@163.com>
2017-09-18 10:34:41 +0800

    add test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @authors liwb (you@example.org)
+ * @date    2017/9/18 10:34
+ * @version $ IIFE
+ */
+
+/* name module */
+
+(function ($, w, d) {
+
+    'use strict';
+
+    /* do something */
+
+}(jQuery, window, document));
