aa2cba8877dd07f7bd1cdc7039dc3ce98e742bc2
cklwblove <lwbhtml@163.com>
2017-10-19 14:24:33 +0800

    release

--- build/release.sh
+++ build/release.sh
@@ -2,7 +2,7 @@
 
 npm run build && npm run docs:deploy
 git add -A
-git commit -m "[build] 1.1.0"
-npm version 1.1.1 --message "[release] 1.1.0"
+git commit -m "[build] 1.1.2"
+npm version 1.1.2 --message "[release] 1.1.2"
 
 git push
